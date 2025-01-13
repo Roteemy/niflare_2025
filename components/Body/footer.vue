4383d40b1fb23d1497b3789fc2b5633794cca27c
Roteemy <83503420+Roteemy@users.noreply.github.com>
2025-01-13 15:13:18 -0500

    Update footer.vue

--- components/Body/footer.vue
+++ components/Body/footer.vue
@@ -1,35 +1,30 @@
 <template>
-    <div class="flex justify-center mt-40">
-        <div class="Changer basis-9/12 rounded-3xl py-20 -mb-48">
-            <div class="text-center font-bold text-7xl">What are you waiting</div>
-            <div class="text-center font-bold text-7xl">for?</div>
+    <div class=" mt-20">
+        <div class="bg-cyan-950  rounded-3xl py-20 -mb-48">
+            <div class="text-center text-white font-bold text-5xl">What are you waiting for?</div>
 
             <div class="py-10">
-                <div class="text-center text-2xl text-gray-800">Stay ahead of the curve with AI-driven technologies</div>
-                <div class="text-center text-2xl text-gray-800">we specialize in creating intelligent applications that
-                </div>
-                <div class="text-center text-2xl text-gray-800">enhance productivity and decision-making.</div>
+                <div class="text-center text-2xl text-white">Stay ahead of the curve with AI-driven technologies we specialize in creating intelligent applications that
+enhance productivity and decision-making.</div>
             </div>
 
             <div class="flex justify-center">
                 <button class="bg-blue-950 hover:bg-gray-900 font-bold py-6 px-12 rounded-full text-white text-2xl">
-                    Download Courses
+                    Contact Us
                 </button>
             </div>
         </div>
     </div>
 
-    <div class="Changera2 flex rounded-t-3xl">
-        <div class="mt-72 px-20 mx-auto w-[90%]">
+    <div class=" flex rounded-t-3xl">
+        <div class="mt-72 px-10 mx-auto ">
 
             <footer class="text-white">
-                <div class="container mx-auto grid grid-cols-6 md:grid-cols-6">
+                <div class="flex flex-wrap space-x-4">
                     <!-- Browse Section -->
-                    <div class="col-span-2">
+                    <div class="">
                         <div class="flex space-x-6">
-                            <div class="">
-                            <img src="/home-hero-img.png" alt="Logo" class="w-12 h-12 mb-4">
-                            </div>
+                  
                             <h3 class="text-lg font-semibold">Browse
                         
                         <ul class="space-y-2 mt-4">
@@ -42,71 +37,41 @@
                 </div>
                     </div>
                     <!-- Company Section -->
-                    <div class="col-span-1">
-                        <h3 class="text-lg font-semibold  ml-6 mb-4">Company</h3>
+                    <div class="">
+                        <h3 class="text-lg font-semibold  ml-6 mb-4">Connect</h3>
                         <ul class="space-y-2">
-                            <li><a href="#niflare" class="hover:underline  ml-6">Niflare</a></li>
-                            <li><a href="#ourservice" class="hover:underline ml-6">Our Service</a></li>
-                            <li><a href="#learnwithus" class="hover:underline ml-6">Learn With Us</a></li>
-                            <li><a href="#" class="hover:underline ml-6">Download Course</a></li>
+                            <li><a href="#niflare" class="hover:underline  ml-6">Facebook</a></li>
+                            <li><a href="#ourservice" class="hover:underline ml-6">Twitter</a></li>
+                            <li><a href="#learnwithus" class="hover:underline ml-6">Email</a></li>
+                            <li><a href="#" class="hover:underline ml-6">WhatsApp</a></li>
                         </ul>
                     </div>
-                    <!-- Follow and Newsletter Section -->
-                    <div class="col-span-1 ml-6">
-                        <h3 class="text-lg font-semibold mb-4">Follow</h3>
-                        <ul class="space-y-2">
-                            <li><a href="#" class="hover:underline">LinkedIn</a></li>
-                            <li><a href="#" class="hover:underline">Twitter</a></li>
-                            <li><a href="#" class="hover:underline">Instagram</a></li>
-                            <li><a href="#" class="hover:underline">Facebook</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-span-2">
-                        <h3 class="text-lg font-semibold mb-2">Sign up to paticipate in our class today
-                            <div class="mt-4">
-                            <input id="email" type="email" placeholder="Email address" class="bg-black w-[90%] border border-gray-100 rounded-full py-3 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-700">
-                        </div>
-                        </h3>
-                    </div>
+              
                 </div>
             </footer>
 
-            <div class="grid grid-cols-2 mt-14">
-                <div class="col-span-1 text-white">
-                    <div class="flex justify-between">
+            <div class="">
+                
+                    <div class="flex justify-center space-x-10 my-10">
                         <p>Privacy Policy</p>
                         <p>Terms of Use</p>
-                        <p>Cookies Policies</p>
+
                     </div>
-                </div>
-                <div class="col-span-1 text-white">
-                    <div class="flex justify-end">
-                        <p>All rights reserved © 2024 Changera</p>
+                
+                <div class=" text-white">
+                    <div class="flex justify-center mb-10">
+                        <p>All rights reserved © 2025 Niflare</p>
                     </div>
                 </div>
             </div>
 
-            <hr class="border-t border-gray-600 mr-4 ml-4 py-2 mt-24 w-12/12" />
 
-
-            <p class="text-white text-lg mt-8">In today's digital era, having an online presence is crucial. We design and
-                develope stunning websites and mobile apps </p>
-            <p class="text-white text-lg text-center">tailored to your business needs.</p>
-
-            <p class="text-white text-lg mt-8 text-center">Success in trading requires stratedy. We develope advanced
-                trading strategies and custom indicators to give you a</p>
-            <p class="text-white text-lg text-center">Competitive edge.</p>
-
-            <p class="text-white text-lg mt-4 text-center mb-10">Empower yourself with the knowledge to excel in technology
-                and trading. Our training programs are designed for individuals of all skill levels.</p>
         </div>
     </div>
 </template>
 
 <style>
-.Changer {
-    background-color: #f6de52;
-}
+
 
 .Changera2 {
     background-color: black;
