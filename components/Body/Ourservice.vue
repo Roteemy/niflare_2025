244ea8f30763cc93aa793c12aaf68d075d9d64dc
Roteemy <83503420+Roteemy@users.noreply.github.com>
2025-01-13 15:11:48 -0500

    Update Ourservice.vue

--- components/Body/Ourservice.vue
+++ components/Body/Ourservice.vue
@@ -1,24 +1,22 @@
 <template>
     <div class="mt-4 mb-20">
         <div class="basis-full">
-            <div class="flex justify-center sm:px-10">
-                <nuxtLink to="/Homepage">
-                <div id="ourservice"
-                    class="h-24 w-64 text-center rounded-full bg-gray-300 flex items-center justify-center uppercase text-3xl font-bold">
-                    Our Service</div>
-                    </nuxtLink>
-            </div>
+            <!--<div class="flex justify-center sm:px-10">-->
+                
+            <!--    <div id="ourservice"-->
+            <!--        class="h-24 w-64 text-center rounded-full bg-gray-300 dark:bg-amber-800 flex items-center justify-center uppercase text-3xl font-bold">-->
+            <!--        Our Service</div>-->
+                    
+            <!--</div>-->
             <div class="mt-10 sm:px-10">
-                <div class="mx-auto w-[65%]">
+                <div class="mx-auto w-[90%]">
                 <p id="webdevelopment" class="text-center text-5xl font-bold mb-6">Web & App Development</p>
-                <p class="text-center">In today’s digital era, having an online presence is crucial. Wedesign and
-                    develop
-                </p>
-                <p class="text-center mb-16">stunning websites and mobile apps tailored to your busine-ss needs.</p>
+                <p class="text-center">In today’s digital era, having an online presence is crucial. We design and
+                    develop stunning websites and mobile apps tailored to your busine-ss needs.</p>
                 </div>
 
-                <div class="flex justify-between space-x-10 mt-8 mx-auto w-[65%]">
-                    <div class="basis-1/2">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:space-y-10 mt-8 w-[95%]">
+                    <div class="px-4">
                         <div class="ml-5">
                             <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -82,9 +80,7 @@
                             <div class="">Progressive Web Apps (PWAs)</div>
                         </div>
 
-                    </div>
-
-                    <div class="basis-1/2">
+                    
                         <div class="ml-2">
                             <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -154,14 +150,13 @@
                     </div>
                 </div>
 
-                <div class="mx-auto w-[80%] mt-20 bg-gray-800 py-6 rounded-3xl sm:px-10">
+                <div class="mt-20 bg-gray-800 py-6 rounded-3xl sm:px-10">
                     <p id="softwaredevelopment" class="text-center text-5xl font-bold mb-6 text-white">Software Development</p>
                     <p class="text-center text-white">We provide bespoke software solutions to streamline your business
-                        processes</p>
-                    <p class="text-center mb-16 text-white">and improve efficiency.</p>
+                        processes and improve efficiency.</p>
 
-                    <div class="flex space-x-10 mt-8 ml-4 mr-4">
-                        <div class="basis-1/2 bg-white rounded-2xl py-2">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  sm:space-x-10 mt-8 ml-4 mr-4">
+                        <div class="dark:bg-gray-900 bg-white rounded-2xl py-2">
                             <div class="ml-5">
                                 <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -217,7 +212,7 @@
                             </div>
                         </div>
 
-                        <div class="basis-1/2 bg-white rounded-2xl py-2">
+                        <div class="dark:bg-gray-900 bg-white rounded-2xl py-2">
                             <div class="ml-5">
                                 <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -262,13 +257,12 @@
                 </div>
 
                 
-                <div class="bg-green-200 mx-auto w-[80%] mt-20 py-6 rounded-3xl ">
+                <div class="bg-green-950 mx-auto  mt-20 py-6 rounded-3xl ">
                     <p id="tradindstrategies" class="text-center text-5xl font-bold mb-6">Trading Strategies & Tools</p>
-                    <p class="text-center">Success in trading requires strategy. We develop advanced trading strategies</p>
-                    <p class="text-center mb-16">and custom indicators to give you a competitive edge.</p>
+                    <p class="text-center">Success in trading requires strategy. We develop advanced trading strategies and custom indicators to give you a competitive edge.</p>
 
-                    <div class="flex space-x-10 mt-8 ml-4 mr-4">
-                        <div class="basis-1/2 bg-white rounded-2xl py-2">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  sm:space-x-10 mt-8 ml-4 mr-4">
+                        <div class=" dark:bg-green-700 bg-white rounded-2xl py-2">
                             <div class="ml-5">
                                 <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
@@ -283,92 +277,4 @@
                                 <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
                                         fill="currentColor" class="size-4">
                                         <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Algorithmic trading strategies</div>
-                            </div>
-
-                            <div class="flex space-x-3 ml-2">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Custom indicators for popular trading platforms (MetaTrader, TradingView,
-                                    etc.)</div>
-                            </div>
-
-                            <div class="flex space-x-3 ml-2">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Market analysis tools</div>
-                            </div>
-
-                            <div class="flex space-x-3 ml-2">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Risk management systems</div>
-                            </div>
-                        </div>
-
-                        <div class="basis-1/2 bg-white rounded-2xl py-2">
-                            <div class="ml-5">
-                                <svg class="w-[40px] h-[40px] text-gray-800 dark:text-white" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                </svg>
-                            </div>
-                            <p class="text-3xl font-bold mt-3 ml-2">Key highlights</p>
-                            <div class="flex space-x-3 ml-2 mt-4">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Backtested and optimized strategies</div>
-                            </div>
-                            <div class="flex space-x-3 ml-2">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Designed for beginner and professional traders alike</div>
-                            </div>
-                            <div class="flex space-x-3 ml-2">
-                                <div class="py-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                                        fill="currentColor" class="size-4">
-                                        <path fill-rule="evenodd"
-                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="">Enhanced decision-making with data-driven insights</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
+                                            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75
